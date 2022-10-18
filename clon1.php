08daae8ee858b467a540a73d6a8b45682e0f68f8
Analia Burgos <analiaveronicadra@gmail.com>
2022-10-18 18:36:22 -0300

    agrego titulos a la pestaña empresa

---
+++ clon1.php
@@ -0,0 +1,42 @@
+<h1> Acerca de la Empresa</h1>
+<h2>Quienes somos</h2>
+
+<img src="https://mejoremprender.com/wp-content/uploads/2017/11/nombres-para-empresas-constructoras-1.jpg" alt=" empresa">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquid ipsum magni quas eaque veritatis, nemo aliquam fuga quod qui nostrum laudantium a voluptas, illum recusandae eveniet. Nulla, voluptate iste?</p>
+
+
+<h4>Algunos de nuestros proyectos:</h4>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.construyehogar.com/wp-content/uploads/2014/08/Dise%C3%B1o-de-casa-moderna-de-una-planta.jpg" class="d-block w-100" alt="casa">
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.pinimg.com/originals/e2/bf/b9/e2bfb93f9ea1d38da47949c2773511a4.jpg" class="d-block w-100" alt="casa">
+    </div>
+    <div class="carousel-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQesS9_a1iytU3R1DH53gwNisxCE0zSfXlvrhDAfapcA49ywsN4kInF0SU49t5pe-s6qoU&usqp=CAU" class="d-block w-100" alt="casa">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
+
+  <?php
+   include "footer.html";
+  ?>
