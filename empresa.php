c5c1c1c32cd8d3a746ece81f6ec6b9cf23553851
Analia Burgos <analiaveronicadra@gmail.com>
2022-10-18 20:11:15 -0300

    agrego rama pruebas

--- empresa.php
+++ empresa.php
@@ -13,10 +13,14 @@
   ?>
 
 <h1> Acerca de la Empresa</h1>
-<h2>Quienes somos</h2>
-<img src="https://mejoremprender.com/wp-content/uploads/2017/11/nombres-para-empresas-constructoras-1.jpg" alt=" empresa">
-<h4>Algunos de nuestros proyectos:</h4>
 
+
+<h2>Quienes somos</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates inventore veniam, fuga distinctio, mollitia nihil facilis, commodi minus voluptatibus dicta ex error nisi dolorem harum temporibus aspernatur ducimus culpa debitis?</p>
+<img src="https://mejoremprender.com/wp-content/uploads/2017/11/nombres-para-empresas-constructoras-1.jpg" alt=" empresa">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquid ipsum magni quas eaque veritatis, nemo aliquam fuga quod qui nostrum laudantium a voluptas, illum recusandae eveniet. Nulla, voluptate iste?</p>
+<h4>Algunos de nuestros proyectos:</h4>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore reprehenderit fuga perspiciatis officiis! Rem, velit odio. Nesciunt, in corrupti iure non rerum enim sed minus neque perferendis? Assumenda, dicta fuga?</p>
 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
