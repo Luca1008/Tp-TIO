c8efebb269c1bc324bcdb8154b3c128ceb38ab27
Analia Burgos <analiaveronicadra@gmail.com>
2022-10-18 18:41:30 -0300

    agrego imagen y descripcion

--- empresa.php
+++ empresa.php
@@ -14,6 +14,9 @@
 
 <h1> Acerca de la Empresa</h1>
 <h2>Quienes somos</h2>
+<img src="https://mejoremprender.com/wp-content/uploads/2017/11/nombres-para-empresas-constructoras-1.jpg" alt=" empresa">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquid ipsum magni quas eaque veritatis, nemo aliquam fuga quod qui nostrum laudantium a voluptas, illum recusandae eveniet. Nulla, voluptate iste?</p>
+
 
   
 </body>
