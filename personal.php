279716936eccbc802265406a57c79ceea2d870a7
Nadrak1 <nicourdu@gmail.com>
2022-10-20 23:24:50 -0300

    agrego file personal y trabajo sobre el

---
+++ personal.php
@@ -0,0 +1,15 @@
+<?php
+require_once "templates/header.php";
+?>
+
+<h1>Nuestro personal</h1>
+<p>Juan Enrique<p>
+<p>Pedro Seguno<p>
+<p>Tomas Struind<p>
+<p>Federico Eriue<p>
+<p>Peuan Muese<p>
+<p>Nicolas Enre<p>
+
+<?php
+require_once "templates/footer.php";
+?>
