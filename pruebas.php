3c72062ed4b6005bae9d02d05a5269aca9abe37f
Analia Burgos <analiaveronicadra@gmail.com>
2022-10-22 08:58:57 -0300

    agrego rama para trabajar en conjunto

---
+++ pruebas.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+include  "header.html";
+?>
+
+<h1> Esta es la rama pruebas donde hay que hacer cambios entre todos</h1>
+
+<p>agrego un parrafo :</p>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis aspernatur nulla laborum id odio porro totam libero, repudiandae, quo ipsum accusantium vero, rerum modi dignissimos iusto? Sapiente amet velit quis!</p>
+
+
+<?php
+include "footer.html"
+
+?>
