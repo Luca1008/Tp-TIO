c043f36c0141d80a29b0d4035e5492fa11e5ce64
Luca1008 <111925104+Luca1008@users.noreply.github.com>
2022-10-29 15:50:41 -0300

    cambio de nombre de proyect

--- proyect.php
+++ proyect.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <header><?php include  "header.html" ?></header>
+
+<?php include  "header.html" ?>
     <main>
         <h1>Nuestros proyectos</h1>
         <article class="artProyect">
@@ -31,7 +23,5 @@
             <p></p>
         </article>
     </main>
-    <footer> <?php include  "footer.html" ?> </footer>
-    
-</body>
-</html>
+<?php include  "footer.html" ?> 
+
