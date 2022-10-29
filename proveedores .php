00835bdb2f87a0a3f3e915cc4729b9d9adcacf82
Luca1008 <111925104+Luca1008@users.noreply.github.com>
2022-10-29 17:02:59 -0300

    Creo una nueva rama para trabajar en proveedores y meto informacion

---
+++ proveedores .php
@@ -0,0 +1,23 @@
+<?php include  "templates/header.php" ?>
+    
+    <h1>Nuestros Proveedores</h1>
+
+    <p>Los siguientes nombrandos son las empresas que nos proveen a nosotros de los recursos basicos,mano de obra y herramientas para llevar a cabo nuestro trabajo</p>
+
+    <article>
+        <h2>Juan carlos Herramientas</h2>
+        <p>Nuestro confiables amigos de juancarlos Herramientas nos dan las mejores maquinarias 
+            con seño de aprobacion de CAFNA
+        </p>
+    </article>
+    <article>
+        <h2>Maderera Pablito</h2>
+        <p>Las mejores maderas ofrecidas exportadas de una isla de anda a saber donde</p>
+    </article>
+    <article>
+        <h2>onii-chan</h2>
+        <p>Las mejores lolis para que nuestro trabajadores se sientan comodos a la hora de trabajar</p>
+    </article>
+
+
+<?php include  "templates/footer.php" ?>
