c8a1dbd79f71ad55786a770e9822f4bd49e7a964
Atilio Luque <atilio6999@gmail.com>
2022-10-31 17:27:23 -0300

    Traduzco el login

--- login.php
+++ login.php
@@ -12,13 +12,13 @@
           <!-- Email input -->
           <div class="form-outline mb-4">
             <input type="email" id="form3Example3" class="form-control" />
-            <label class="form-label" for="form3Example3">Email address</label>
+            <label class="form-label" for="form3Example3">Direccion email</label>
           </div>
 
           <!-- Password input -->
           <div class="form-outline mb-3">
             <input type="password" id="form3Example4" class="form-control" />
-            <label class="form-label" for="form3Example4">Password</label>
+            <label class="form-label" for="form3Example4">Contraseña</label>
           </div>
 
           <div class="d-flex justify-content-between align-items-center">
@@ -26,16 +26,16 @@
             <div class="form-check mb-0">
               <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
               <label class="form-check-label" for="form2Example3">
-                Remember me
+                Recordar
               </label>
             </div>
-            <a href="" class="text-body">Forgot password?</a>
+            <a href="" class="text-body">Olvido su contraseña?</a>
           </div>
 
           <div class="text-center text-lg-start mt-4 pt-2">
-            <button type="submit" class="btn btn-primary btn-block">Login</button>
-            <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="signup.html"
-                class="link-danger">Register</a></p>
+            <button type="submit" class="btn btn-primary btn-block">Iniciar sesion</button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">No tiene cuenta? <a href="signup.html"
+                class="link-danger">Registrarse</a></p>
           </div>
 
         </form>
