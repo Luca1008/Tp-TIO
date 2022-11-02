d756e0f606411e01b1f15d148e85ee9fb081f28c
Martina Diaz <martuudiaz28@gmail.com>
2022-11-02 17:00:05 -0300

    agrego archivo blog

---
+++ blog.php
@@ -0,0 +1,14 @@
+<?php include  "templates/header.php" ?>
+<div class="container about-bg">
+    <h1>Blog</h1>
+    <h2>Experiencia sobre Luca: nuestro cliente más frecuente</h2>
+    <p>
+        "Tuve una muy buena experiencia con la empresa.Siempre predispuestos a ayudarme con lo que necesite
+         y muy atentos con su trabajo"
+    </p>
+    <h2>Experiencia sobre Bernardo: nuestro cliente más fiel</h2>
+    <p>
+        "Perfecto trato hacia el cliente"
+    </p>
+</div>
+<?php include  "templates/footer.php" ?>
